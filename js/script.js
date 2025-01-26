37ff0817586fe77e480e593c122a0d36af257557
Troelsrokkedal <eaa24trrk@students.eaaa.dk>
2025-01-26 18:20:23 +0100

    done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+let fødselsår = prompt("Indtast dit fødselsår:");
+
+// Trin 2: Konverter inputtet til et heltal ved hjælp af parseInt
+fødselsår = parseInt(fødselsår, 10);
+
+// Trin 3: Få det nuværende år ved hjælp af new Date()-funktionen
+const nuværendeÅr = new Date().getFullYear();
+
+// Trin 4: Beregn alderen
+const alder = nuværendeÅr - fødselsår;
+
+// Trin 5: Vis resultatet i konsollen
+console.log(`Du er ${alder} år gammel.`);
